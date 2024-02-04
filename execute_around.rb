60ccb0109fb53ae4c89ef1ec179d24d5bc5817c2
Henry Nawrocki Sztanski <henry@sztanski.com>
2024-02-03 21:41:30 -0500

    Add new methods and code blocks

--- execute_around.rb
+++ execute_around.rb
@@ -1,3 +1,5 @@
+require "thread"
+
 def h1
   return "<h1>#{ yield }</h1>"
 end
@@ -16,11 +18,39 @@ def tag(element)
   print "</#{element}>"
 end
 
-# tag(:h1) { "Breaking News!" }
-# tag(:h2) { "Massive Ruby Discovered" }
+# tag(:ul) do
+#   tag(:li) { "It sparkles!"}
+#   tag(:li) { "It shines!"}
+#   tag(:li) { "It mesmerizes!"}
+# end
 
-tag(:ul) do
-  tag(:li) { "It sparkles!"}
-  tag(:li) { "It shines!"}
-  tag(:li) { "It mesmerizes!"}
+def with_debugging
+  puts "Got Here"
+  puts "Result was #{yield}"
 end
+
+# with_debugging do
+#   magic_number = (23 - Time.now.hour) * Math::PI
+# end
+
+def with_expectation(expected)
+  puts "Running test..."
+  result = yield
+  if result == expected
+    puts "Passed."
+  else
+    puts "Expected #{expected}, but got #{result}"
+  end
+end
+
+# with_expectation(4) { 2 + 2 }
+
+# with_expectation("Henry") { "Hen" + "y" }
+
+# require "benchmark"
+
+# time = Benchmark.realtime do
+#   concurrent_fibonacci(45)
+# end
+
+# p time
